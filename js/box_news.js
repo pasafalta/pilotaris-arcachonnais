90ac56ced995056ff506c017b7036c3d0884c34b
clacombe <pasafalta@gmail.com>
2016-10-13 22:36:44 +0200

    utilisation d'angular grid

--- js/box_news.js
+++ js/box_news.js
@@ -2,25 +2,43 @@
 
 var mainApp = angular.module('mainApp', ['angularGrid']);
 
-mainApp.controller('newsController', function($scope, $http) {
-    $scope.gridWidth = 700;
+mainApp.service('dataService',['$http',function($http){
+        this.loadData = function(){
+            var url = "data/news/2016.txt";
+            return $http.get(url);
+        };
+    }])
+
+mainApp.controller('newsController', ['$scope','dataService','angularGridInstance', function($scope,dataService,angularGridInstance) {
     $scope.angularGridOptions = {
-        gridWidth : 280,
-        gutterSize : 10,
+        gridWidth : 250,
+        gutterSize : 20,
         refreshOnImgLoad : true
     }
-    var url = "data/news/2016.txt";
-    $http.get(url).success( function(response) {
+    dataService.loadData().success(function(response){
         response.forEach(function(obj){
             obj.actualHeight = 100;
             obj.actualWidth = 280;
+            if (typeof(obj.Titre) == "undefined") {
+                obj.Titre = "";
+            };
         });
         $scope.news = response;
+        $scope.allnews = response;
     });
+
+    //apply search on the list base on searchTxt which can be binded to an input element
+    $scope.$watch('searchTxt', function (val) {
+        val = val.toLowerCase();
+        $scope.news = scope.allnews.filter(function (obj) {
+            return obj.Titre.toLowerCase().indexOf(val) != -1;
+        });
+    });
+    
     $scope.refresh = function(){
         angularGridInstance.gallery.refresh();
     };
 
     
     //window.addEventListener("resize", $scope.refresh);
-});
+}]);
